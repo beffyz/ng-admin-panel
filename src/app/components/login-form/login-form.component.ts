af95a2928ee407aba309c433101c1cb258be9073
Bogdans Zabela <bogdanz@inbox.lv>
2022-07-05 17:31:19 +0300

    Created a project, implemented a router and bootstrap, created a login page, added a validation for email and password

---
+++ src/app/components/login-form/login-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss'],
+})
+export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup = this.fb.group({});
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
+      ]),
+    });
+  }
+
+  submitLogin(): void {
+    console.log(this.loginForm.value);
+  }
+}
